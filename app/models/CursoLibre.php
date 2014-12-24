6b8de1d5df69fa54abc391ddb18acdb3e9575eae
Juan Eracleo <juan@escodigo.com>
2014-12-24 10:10:54 -0500

    He cambiado codigo -> id en el metodo agregar

--- app/models/CursoLibre.php
+++ app/models/CursoLibre.php
@@ -3,13 +3,12 @@
 class CursoLibre extends Eloquent {
 
 	protected $table = 'curso_cl';
-	protected $fillable = array('id','codigo','nombre','horas_academicas','estado','updated_at','created_up');
-	
+	protected $fillable = array('id','nombre','horas_academicas','estado','updated_at','created_up');
+
 	public static function agregar($input)
 	{
 		$respuesta = array();
 		$reglas = array(
-				'codigo'=>array('required','max:10'),
 				'nombre'=>array('required','max:30'),
 				'horas_academicas'=>array('required','max:30')
 			);
@@ -19,17 +18,17 @@ class CursoLibre extends Eloquent {
 		{
 			$respuesta['mensaje'] = 'DATOS INGRESADOS INCORRECTAMENTE';
 			$respuesta['error'] = true;
-		} 
+		}
 		else
 		{
 			$curso = new CursoLibre;
-			$curso->codigo = Input::get('codigo');
+			$curso->id = Input::get('id');
 			$curso->nombre = Input::get('nombre');
 			$curso->horas_academicas = Input::get('horas_academicas');
 			$curso->estado = 1;
 			$curso->created_at= time();
 			$curso->updated_at = time();
-			if(!CursoLibre::find(input::get('codigo')))
+			if(CursoLibre::find(input::get('id')))
 			{
 				$respuesta['mensaje'] = 'YA EXISTE ESE CODIGO';
 				$respuesta['error'] = true;
@@ -37,19 +36,19 @@ class CursoLibre extends Eloquent {
 			}
 			else
 			{
-				if ($curso->save()) 
+				if ($curso->save())
 				{
 					$respuesta['mensaje'] = 'Curso Creado';
 					$respuesta['error'] = false;
-					$respuesta['data'] = $curso;				
+					$respuesta['data'] = $curso;
 				}
-				else 
+				else
 				{
 					$respuesta['mensaje'] = 'DATOS INCORRECTOS';
 					$respuesta['error'] = true;
-					$respuesta['data'] = $curso;				
-				}	
-			}		
+					$respuesta['data'] = $curso;
+				}
+			}
 		}
 		return $respuesta;
 	}
--- app/models/CursoTecnico.php
+++ app/models/CursoTecnico.php
@@ -4,12 +4,11 @@ class CursoTecnico extends Eloquent {
 
 	protected $table = 'curso_ct';
 
-	protected $fillable = array('id','codigo','nombre','modulo','estado','horas_academicas','codCarrera','updated_at','created_at');
+	protected $fillable = array('id','nombre','modulo','estado','horas_academicas','codCarrera','updated_at','created_at');
 	public static function agregar($input)
 	{
 		$respuesta = array();
 		$reglas = array(
-			'codigo'=>array('required','max:10'),
 			'nombre'=>array('required','max:30'),
 			'modulo'=>array('required','max:2'),
 			'horas_academicas'=>array('required','max:30')
@@ -19,40 +18,40 @@ class CursoTecnico extends Eloquent {
 		{
 			$respuesta['mensaje'] = 'DATOS INGRESADOS INCORRECTAMENTE';
 			$respuesta['error'] = true;
-		} 
+		}
 		else
-		{	
+		{
 			$curso = new CursoTecnico;
-			$curso->codigo = Input::get('codigo');
+			$curso->id = Input::get('id');
 			$curso->nombre = Input::get('nombre');
 			$curso->modulo = Input::get('modulo');
 			$curso->horas_academicas = Input::get('horas_academicas');
 			$curso->estado = 1;
 			$curso->codCarrera = Input::get('codCarrera');
 			$curso->created_at= time();
-			$curso->updated_at = time();		
-			if(CursoTecnico::find(input::get('codigo')))
+			$curso->updated_at = time();
+			if(CursoTecnico::find(input::get('id')))
 			{
 				$respuesta['mensaje'] = 'YA EXISTE ESE CODIGO';
 				$respuesta['error'] = true;
 				$respuesta['data'] = $curso;
 			}
 			else
-			{				
-				if ($curso->save()) 
+			{
+				if ($curso->save())
 				{
 					$respuesta['mensaje'] = 'Curso Creado';
 					$respuesta['error'] = false;
 					$respuesta['data'] = $curso;
-				
+
 				}
-				else 
+				else
 				{
 					$respuesta['mensaje'] = 'DATOS INCORRECTOS';
 					$respuesta['error'] = true;
 					$respuesta['data'] = $curso;
-				
-				}	
+
+				}
 			}
 		}
 		return $respuesta;
