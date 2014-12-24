0da58314ddcc38c61bb41e1cf9fc2a03bb58d855
Juan Eracleo <juan@escodigo.com>
2014-12-24 10:15:57 -0500

    procedimiento actualizado codAlumno->id

--- procedimientosingresodenotas.sql
+++ procedimientosingresodenotas.sql
@@ -3,8 +3,8 @@ DELIMITER $$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `AlumnosXCursoCT`(IN `codcargaAcademica` VARCHAR(20))
 BEGIN
-    select N.id as idNota, A.codAlumno as "idAlumno", CONCAT(A.apellidos,  ' ', A.nombre) as "NombreCpt", N.notaa as "Nota1", N.notab as "Nota2", N.notac as "Nota3"
-	from (alumno A inner join matricula_ct M on A.codAlumno = M.codAlumno) inner join nota_ct N on N.codMatricula_ct = M.id
+    select N.id as idNota, A.id as "idAlumno", CONCAT(A.apellidos,  ' ', A.nombre) as "NombreCpt", N.notaa as "Nota1", N.notab as "Nota2", N.notac as "Nota3"
+	from (alumno A inner join matricula_ct M on A.id = M.codAlumno) inner join nota_ct N on N.codMatricula_ct = M.id
     where M.codCargaAcademica_ct = codcargaAcademica
     ORDER BY A.apellidos;
 END$$
@@ -26,8 +26,8 @@ DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `AlumnosXCursoCL`(IN `codcargaAcademica` VARCHAR(20))
     NO SQL
 BEGIN
-    select N.id as idNota, A.codAlumno as "idAlumno", CONCAT(A.apellidos,  ' ', A.nombre) as "NombreCpt", N.nota as "Nota"
-	from (alumno A inner join matricula_cl M on A.codAlumno = M.codAlumno) inner join nota_cl N on N.codMatricula_cl = M.id
+    select N.id as idNota, A.id as "idAlumno", CONCAT(A.apellidos,  ' ', A.nombre) as "NombreCpt", N.nota as "Nota"
+	from (alumno A inner join matricula_cl M on A.id = M.codAlumno) inner join nota_cl N on N.codMatricula_cl = M.id
     where M.codCargaAcademica_cl = codcargaAcademica
     ORDER BY A.apellidos;
 END$$
