81b8a2b3b53c37d7477e5c24a18bd0ad153a561f
AyLGroup <lesly7231@gmail.com>
2014-12-26 14:19:43 -0500

    e corregido el modelo con validaciones

--- app/models/SilaboCursoLibre.php
+++ app/models/SilaboCursoLibre.php
@@ -9,45 +9,64 @@ class SilaboCursoLibre extends Eloquent {
 	{
 		$respuesta = array();
 		$reglas = array(
-				'titulo'=>array('required','max:20'),
+				'capitulo'=>array('required','max:50'),
+				'titulo'=>array('required','max:120'),
+				'numeroclases'=>array('required','max:11'),
 				'orden'=>array('required','max:11')
 			);
 
 		$validador = Validator::make($input,$reglas);
 		if($validador->fails())
 		{
-			$respuesta['mensaje'] = 'DATOS INGRESADOS INCORRECTAMENTE';
+			$respuesta['mensaje'] = 'Datos Ingresados Incorrectamente';
+			$respuesta['error'] = true;
+			$respuesta['data'] = $input;
+		} 
+		else
+		{
+			$nombre = SilabusCL::get()->last();	
+			$a = $nombre->id;
+			$silaboaux = new SilaboCursoLibre;
+			$silaboaux->codSilabus_cl =$a;
+			$silaboaux->capitulo = Input::get('capitulo');
+			$silaboaux->titulo = Input::get('titulo');
+			$silaboaux->objetivos = Input::get('objetivos');			
+			$silaboaux->descripcion = Input::get('descripcion');
+			$silaboaux->numeroclases = Input::get('numeroclases');
+			$silaboaux->orden = Input::get('orden');
+			$silaboaux->estado = 1;
+			$silaboaux->created_at= time();
+			$silaboaux->updated_at = time();
+			$silaboaux->save();
+			
+			$respuesta['mensaje'] = 'Silabo Creado';
+			$respuesta['error'] = false;		
+
+		}
+		return $respuesta;
+			
+	}
+	public static function editar($input)
+	{
+		$respuesta = array();
+		$reglas = array(
+				'capitulo'=>array('required','max:50'),
+				'titulo'=>array('required','max:120'),
+				'numeroclases'=>array('required','max:11'),
+				'orden'=>array('required','max:11')
+		);
+		$validador = Validator::make($input,$reglas);
+		if($validador->fails())
+		{
+			$respuesta['mensaje'] = 'Datos Ingresados Incorrectamente';
 			$respuesta['error'] = true;
 		} 
 		else
 		{
-			$silabo = new SilaboCursoLibre;
-			$silabo->codSilabus_cl = $input::get('id');
-			$silabo->capitulo = Input::get('capitulo');
-			$silabo->titulo = Input::get('titulo');
-			$silabo->objetivos = Input::get('objetivos');			
-			$silabo->descripcion = Input::get('descripcion');
-			$silabo->numeroclases = Input::get('numeroclases');
-			$silabo->orden = Input::get('orden');
-			$silabo->estado = 1;
-			$silabo->created_at= time();
-			$silabo->updated_at = time();
-			//$curso->save();
-			if ($silabo->save()) 
-			{
-							$respuesta['mensaje'] = 'Silabo Creado';
+			$respuesta['mensaje'] = 'Silabo Actualizado';
 			$respuesta['error'] = false;
-			$respuesta['data'] = $silabo;
-			}
-			else 
-			{
-							$respuesta['mensaje'] = 'Silabo mal Creado';
-			$respuesta['error'] = false;
-			$respuesta['data'] = $silabo;
-			}
-
-			
 		}
 		return $respuesta;
 	}
 }
+
