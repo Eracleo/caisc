4699bfbc4dbfb315867869d20f53723822c46b1c
AyLGroup <lesly7231@gmail.com>
2014-12-26 14:19:49 -0500

    e corregido el modelo con validaciones

--- app/models/SilaboCursoTecnico.php
+++ app/models/SilaboCursoTecnico.php
@@ -9,44 +9,62 @@ class SilaboCursoTecnico extends Eloquent {
 	{
 		$respuesta = array();
 		$reglas = array(
-				'titulo'=>array('required','max:20'),
+				'capitulo'=>array('required','max:50'),
+				'titulo'=>array('required','max:120'),
+				'numeroclases'=>array('required','max:11'),
 				'orden'=>array('required','max:11')
 			);
 
 		$validador = Validator::make($input,$reglas);
 		if($validador->fails())
 		{
-			$respuesta['mensaje'] = 'DATOS INGRESADOS INCORRECTAMENTE';
+			$respuesta['mensaje'] = 'Datos Ingresados Incorrectamente';
+			$respuesta['error'] = true;
+			$respuesta['data'] = $input;
+		} 
+		else
+		{
+			$nombre = SilabusCT::get()->last();	
+			$a = $nombre->id;
+			$silabo = new SilaboCursoTecnico;
+			$silabo->codSilabus_ct =$a;
+			$silabo->capitulo = Input::get('capitulo');
+			$silabo->titulo = Input::get('titulo');
+			$silabo->objetivos = Input::get('objetivos');			
+			$silabo->descripcion = Input::get('descripcion');
+			$silabo->numeroclases = Input::get('numeroclases');
+			$silabo->orden = Input::get('orden');
+			$silabo->estado = 1;
+			$silabo->created_at= time();
+			$silabo->updated_at = time();
+			$silabo->save();			
+			$respuesta['mensaje'] = 'Silabo Creado';
+			$respuesta['error'] = false;	
+			$respuesta['data'] = $silabo;
+			
+		}
+		return $respuesta;
+	}
+
+	public static function editar($input)
+	{
+		$respuesta = array();
+		$reglas = array(
+				'capitulo'=>array('required','max:50'),
+				'titulo'=>array('required','max:120'),
+				'numeroclases'=>array('required','max:11'),
+				'orden'=>array('required','max:11')
+		);
+		$validador = Validator::make($input,$reglas);
+		if($validador->fails())
+		{
+			$respuesta['mensaje'] = 'Datos Ingresados Incorrectamente';
 			$respuesta['error'] = true;
 		} 
 		else
 		{
-			$silabo = new SilaboCursoTecnico;
-			$silabo->codSilabus_cl = Input::get('codSilabus_cl');
-			$silabo->capitulo = Input::get('capitulo');
-			$silabo->titulo = Input::get('titulo');
-			$silabo->objetivos = Input::get('objetivos');
-			
-			$silabo->descripcion = Input::get('descripcion');
-			$silabo->numeroclases = Input::get('numeroclases');
-			$silabo->orden = Input::get('orden');
-			$silabo->created_at= time();
-			$silabo->updated_at = time();
-			//$silabo->save();
-			if ($silabo->save()) 
-			{
-				Session::flash('message','Guardado correctamente!');
-				Session::flash('class','success');
-			}
-			else 
-			{
-				Session::flash('message','Ha ocurrido un error!');
-				Session::flash('class','danger');
-			}
-			$respuesta['mensaje'] = 'silabo Creado';
+			$respuesta['mensaje'] = 'Silabo Actualizado';
 			$respuesta['error'] = false;
-			$respuesta['data'] = $silabo;
-			
 		}
 		return $respuesta;
 	}
