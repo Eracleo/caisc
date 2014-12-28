2814c92ebc2768ffd5054dd2b6b9f4350c15a919
fryzito <fryzito@users.noreply.github.com>
2014-12-28 16:42:40 -0500

    Actualizando vista CP index

--- app/views/CarreraProfesional/index.blade.php
+++ app/views/CarreraProfesional/index.blade.php
@@ -5,7 +5,21 @@ Listar Carreras Profesionales
 @section('options')
 <li>CARRERAS PROFESIONALES</li>
 @stop
+<style>
+    span {
+        margin: 5px;
+    }
+    span a{
+        color: white;
+    }
+</style>
 @section('content')
+        <div class="col-xs-6">
+                    <div id="example1_filter" class="dataTables_filter">
+                        <label>Buscar: <input aria-controls="example1" type="text"></label>
+                        <a href="CarreraProfesional/add.html"><span class="label label-success">AGREGAR CARRERA</a>
+                    </div>
+        </div>
     <table class="table table-bordered table-striped dataTable">
         <thead>
             <tr role="row">
@@ -22,9 +36,9 @@ Listar Carreras Profesionales
                 <td class=" ">{{ $dato->nombre }}</td>
                 <td class=" ">{{ $dato->descripcion }}</td>
                 <td class=" ">
-                    {{ HTML::link('CarreraProfesional/updatecID/'.$dato->id,'Actualizar') }}
-                    {{ HTML::link('CarreraProfesional/post_eliminar/'.$dato->id,'Eliminar') }}
-                    {{ HTML::link('CarreraProfesional/profile/'.$dato->id,'Detalles') }}
+                    <span class="label label-warning">{{ HTML::link('CarreraProfesional/updatecID/'.$dato->id,'Actualizar') }}</span>
+                    <span class="label label-danger">{{ HTML::link('CarreraProfesional/post_eliminar/'.$dato->id,'Eliminar') }}</span>
+                    <span class="label label-primary">{{ HTML::link('CarreraProfesional/profile/'.$dato->id,'Detalles') }}</span>
                 </td>
         </tr>
         @endforeach
