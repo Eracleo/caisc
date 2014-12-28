b327e4c2531b15ce72aaee03efe12986aec76c71
fryzito <fryzito@users.noreply.github.com>
2014-12-28 17:41:10 -0500

    Validaciones Carrera profesional

--- app/controllers/CarreraProfesionalController.php
+++ app/controllers/CarreraProfesionalController.php
@@ -1,5 +1,4 @@
 
-
 <?php
 
 class CarreraProfesionalController extends BaseController
@@ -63,25 +62,34 @@ class CarreraProfesionalController extends BaseController
 	}
 	public function post_actualizar()
 	{
-		$cod=Input::get('id');
-
-		if(is_null($cod))
+		$respuesta = Carrera::editar(Input::all());
+		if($respuesta['error']==true)
+		{	$id = input::get('id');
+			$carrera = Carrera::where('id','=',$id)->firstOrFail();
+			return Redirect::to('CarreraProfesional/updatecID/'.$id)->with('mensaje',$respuesta['mensaje'])->withInput();
+		}
+		else
 		{
-			Redirect::to('404.html');
-		} 
-		else 
-		{
-			$carrera = Carrera::where('id','=',$cod)->firstOrFail();
-			if(is_object($carrera))
+			$cod=Input::get('id');
+			if(is_null($cod))
 			{
-				$carrera->id = Input::get('id');
-				$carrera->nombre = Input::get('nombre');
-				$carrera->descripcion = Input::get('descripcion');
-				$carrera->updated_at = time();
-				$carrera->save();
-				return Redirect::to('CarreraProfesional');
-			} else {
-				Redirect::to('500.html');
+				Redirect::to('404.html');
+			} 
+			else 
+			{
+				$carrera = Carrera::where('id','=',$cod)->firstOrFail();
+				if(is_object($carrera))
+				{
+					$carrera->nombre = Input::get('nombre');
+					$carrera->descripcion = Input::get('descripcion');
+					$carrera->updated_at = time();
+					$carrera->save();
+					return Redirect::to('CarreraProfesional');
+				} 
+				else 
+				{
+					Redirect::to('500.html');
+				}
 			}
 		}
 	}
@@ -97,35 +105,7 @@ class CarreraProfesionalController extends BaseController
 			return View::make('CarreraProfesional.post_eliminar',array('carrera'=>$carrera));		
 		}
 	}
-	public function update($id=0)
-	{
-		$respuesta = Docente::editar(Input::all());
-		if($respuesta['error']==true)
-		{
-			return Redirect::to('docente/edit/'.$id)->withErrors($respuesta['mensaje'] )->withInput();
-		}
-		else
-		{
-			if($id<1)
-			{
-				Redirect::to('404.html');
-			} else {
-				$docente = Docente::where('id','=',$id)->firstOrFail();
-				if(is_object($docente))
-				{
-					$docente->nombre = Input::get('nombre');
-					$docente->apellidos = Input::get('apellidos');
-					$docente->dni = Input::get('dni');
-					$docente->email = Input::get('email');
-					$docente->telefono = Input::get('telefono');
-					$docente->save();
-					return Redirect::to('docente/profile/'.$id);
-				} else {
-					Redirect::to('500.html');
-				}
-			}
-		}
-	}
+
 	public function delete($id = null)
 	{
 		if(is_null($id))
@@ -140,33 +120,6 @@ class CarreraProfesionalController extends BaseController
 				return Redirect::to('CarreraProfesional');
 			}
 		}
-	}
-	public function imagen($id=null)
-	{
-		if(is_null($id) or ! is_numeric($id))
-		{
-			return Redirect::to('404.html');
-		} else {
-			$docente = Docente::where('id','=',$id)->firstOrFail();
-			return View::make('docente.imagen',array('docente'=>$docente));
-		}
-	}
-	public function uploadImage($id=null)
-	{
-		$mensaje = "Ocurrio Error";
-		if(Input::file("foto")->isValid())
-		{
-			$file = Input::file("foto");
-			$fileName = Input::file('foto')->getClientOriginalName();
-			$docente = Docente::where('id','=',$id)->firstOrFail();
-			$docente->foto = md5($id."-".$fileName).'.'.Input::file('foto')->getClientOriginalExtension();
-			if($docente->save())
-			{
-				Input::file('foto')->move('assets/foto',$docente->foto);
-				$mensaje = "Imagen actualizado";
-			}
-		}
-		return Redirect::to('docente/profile/'.$id)->withErrors($mensaje);
 	}
 	public function eliminando()
 	{
