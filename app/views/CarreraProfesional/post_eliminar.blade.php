2779a748bc7c55f0b835919527c3cdfef24e9ab6
Gerar Francis <fryzito@users.noreply.github.com>
2015-01-02 01:16:56 -0500

    Actualizando vista post_eliminar CP

--- app/views/CarreraProfesional/post_eliminar.blade.php
+++ app/views/CarreraProfesional/post_eliminar.blade.php
@@ -1,38 +1,37 @@
 @extends('layouts.base_admin')
 @section('title')
-Eliminar <small>CARRERAS PROFESIONALES </small>
+Eliminar <small>CARRERA PROFESIONAL </small>
 @stop
-@section('breadcrumb')
-<li>Carreras Profesionales</li>
+@section('options')
 @stop
 @section('content')
 {{ Form::open(array('method'=> 'POST','url'=> 'CarreraProfesional/delete','class'=>'form-horizontal','role'=>'form')) }}
 <div class="form-group">
 		{{ Form::label('id','Esta seguro de eliminar esta carrera profesional:',array('class'=>'col-sm-4 control-label')) }}
-		<div class="col-sm-8">
+		<div class="col-sm-2">
 			{{ Form::text('id',$carrera->id,array('class'=>'form-control','placeholder'=>'','readonly'=>'readonly'))}}
 		</div>
 	</div>
 
 	<div class="form-group">
 		{{ Form::label('nombre','Nombre de la carrera:',array('class'=>'col-sm-4 control-label')) }}
-		<div class="col-sm-8">
+		<div class="col-sm-2">
 			{{ Form::text('nombre',$carrera->nombre,array('class'=>'form-control','placeholder'=>'','readonly'=>'readonly'))}}
 		</div>
 	</div>
 	<div class="form-group">
 		{{ Form::label('descripcion','Descripcion:',array('class'=>'col-sm-4 control-label')) }}
-		<div class="col-sm-8">
+		<div class="col-sm-4">
 			{{ Form::text('decripcion',$carrera->descripcion,array('class'=>'form-control','placeholder'=>'','readonly'=>'readonly'))}}
 		</div>
 	</div>
 
 
 	<div class="form-group">
-		<div class="col-xs-12 col-sm-6 col-md-6">
-			<button class="btn btn-info btn-block" type="reset">{{ HTML::link('CarreraProfesional','Cancelar') }}</button>
+		<div class="col-xs-12 col-sm-4 col-md-2">
+			{{HTML::linkAction('CarreraProfesionalController@index', 'Cancelar','',array('class'=>'col-sm-12 btn btn-warning'))}}
 		</div>
-		<div class="col-xs-12 col-sm-6 col-md-6">
+		<div class="col-xs-12 col-sm-4 col-md-2">
 			<button class="btn btn-primary btn-block" type="submit">Eliminar</button>
 		</div>
 	</div>
