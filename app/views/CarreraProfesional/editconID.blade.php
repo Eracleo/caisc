dd89cacef7c9f1168887b9a507e96a2908c09fd5
Gerar Francis <fryzito@users.noreply.github.com>
2015-01-02 01:14:40 -0500

    Actualizando vista edit CP

--- app/views/CarreraProfesional/editconID.blade.php
+++ app/views/CarreraProfesional/editconID.blade.php
@@ -2,38 +2,51 @@
 @section('title')
 Actualizar Carrera Profesional: <small> {{$carrera->nombre}} </small>
 @stop
-
-@section('breadcrumb')
-<li>Carreras Profesionales</li>
-<li>Actualizar Carrera Profesional</li>
+@section('options')
+<li>{{HTML::link('CarreraProfesional','Listar')}}</li>
+<li>{{HTML::link('CarreraProfesional/add.html','Nueva Carrera')}}</li>
 @stop
-
 @section('content')
-<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+<div class="col-xs-12 col-sm-12">
 {{ Form::open(array('method'=> 'POST','url'=> 'CarreraProfesional/post_update.html','class'=>'form-horizontal','role'=>'form')) }}
 	<div class="form-group">
-		{{ Form::label('nombre','Nombre Carrera',array('class'=>'col-sm-4 control-label')) }}
-		<div class="col-sm-8">
-			{{ Form::text('nombre',$carrera->nombre,array('class'=>'form-control','placeholder'=>''))}}
-		</div>
-	</div>
-	<div class="form-group">
-		{{ Form::label('descripcion','Descripcion:',array('class'=>'col-sm-4 control-label')) }}
-		<div class="col-sm-8">
-			{{ Form::text('descripcion',$carrera->descripcion,array('class'=>'form-control','placeholder'=>''))}}
-		</div>
-	</div>
-	<div class="form-group">
-		{{ Form::label('id','ID:',array('class'=>'col-sm-4 control-label')) }}
-		<div class="col-sm-8">
+		{{ Form::label('id','ID:',array('class'=>'col-sm-2 control-label')) }}
+		<div class="col-sm-4">
 			{{ Form::text('id',Carrera::find($carrera->id)->id,array('class'=>'form-control','placeholder'=>'','readonly'=>'readonly'))}}
 		</div>
 	</div>
 	<div class="form-group">
-		<div class="col-xs-12 col-sm-6 col-md-6">
-			<button class="btn btn-info btn-block" type="reset">Cancelar</button>
+		{{ Form::label('nombre','Nombre Carrera',array('class'=>'col-sm-2 control-label')) }}
+		<div class="col-sm-4">
+			{{ Form::text('nombre',$carrera->nombre,array('class'=>'form-control','placeholder'=>''))}}
 		</div>
-		<div class="col-xs-12 col-sm-6 col-md-6">
+		<div class="errores">
+			@if ( $errors->has('nombre'))
+		       	@foreach ($errors->get('nombre') as $error)
+			   	<div class="alert alert-danger">* {{ $error }}</div>
+		    	@endforeach
+			@endif
+		</div>
+	</div>
+	<div class="form-group">
+		{{ Form::label('descripcion','Descripcion:',array('class'=>'col-sm-2 control-label')) }}
+		<div class="col-sm-4">
+			{{ Form::text('descripcion',$carrera->descripcion,array('class'=>'form-control','placeholder'=>''))}}
+		</div>
+		<div class="errores">
+			@if ( $errors->has('descripcion'))
+		       	@foreach ($errors->get('descripcion') as $error)
+			   	<div class="alert alert-danger">* {{ $error }}</div>
+		    	@endforeach
+			@endif
+		</div>
+	</div>
+	
+	<div class="form-group">
+		<div class="col-xs-12 col-sm-3">
+			{{HTML::linkAction('CarreraProfesionalController@index', 'Cancelar','',array('class'=>'col-sm-12 btn btn-warning'))}}
+		</div>
+		<div class="col-xs-12 col-sm-3">
 			<button class="btn btn-primary btn-block" type="submit">Actualizar</button>
 		</div>
 	</div>
