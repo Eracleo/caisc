5630df19806281971d1af628aa65086a1e88ad26
fryzito <fryzito@users.noreply.github.com>
2015-01-01 23:08:53 -0500

    Actualizando vista index

--- app/views/Aula/index.blade.php
+++ app/views/Aula/index.blade.php
@@ -1,68 +1,40 @@
 @extends('layouts.base_admin')
 @section('title')
-Listar Aulas
+Lista <small> AULAS </small>
 @stop
-@section('breadcrumb')
-<li>AULAS</li>
+@section('options')
+<li>{{HTML::link('Aula','Listar')}}</li>
+<li>{{HTML::link('Aula/add.html','Nueva Aula')}}</li>
 @stop
-<style>
-    span {
-        margin: 5px;
-    }
-    span a{
-        color: white;
-    }
-</style>
 @section('content')
 <div class="box">
     <div class="box-header">
         <h3 class="box-title"> </h3>
     </div>
-    <div class="box-body table-responsive">
-        <div id="example1_wrapper" class="dataTables_wrapper form-inline" role="grid">
-            <div class="row">
-                <div class="col-xs-6">
-                    <div class="dataTables_length" id="example1_length">
-                        <label><select aria-controls="example1" size="1" name="example1_length">
-                            <option selected="selected" value="10">10</option>
-                            <option value="25">25</option>
-                            <option value="50">50</option>
-                            <option value="100">100</option>
-                        </select> records per page</label>
-                    </div>
-                </div>
-                <div class="col-xs-6">
-                    <div id="example1_filter" class="dataTables_filter">
-                        <label>Buscar: <input aria-controls="example1" type="text"></label>
-                        <a href="Aula/add.html"><span class="label label-success">AGREGAR AULA</a>
-                    </div>
-                </div>
-            </div>
-            <table aria-describedby="example1_info" id="example1" class="table table-bordered table-striped dataTable">
-                <thead>
-                    <tr role="row">
-                        <th aria-label="Rendering engine: activate to sort column descending" aria-sort="ascending" style="width: 80px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" role="columnheader" class="sorting_asc">Codigo de Aula</th>
-                        <th aria-label="Browser: activate to sort column ascending" style="width: 283px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" role="columnheader" class="sorting">Capacidad</th>
-                        <th aria-label="CSS grade: activate to sort column ascending" style="width: 114px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" role="columnheader" class="sorting">Actions</th>
-                    </tr>
-                </thead>
+    <table aria-describedby="example1_info" id="example1" class="table table-bordered table-striped dataTable">
+        <thead>
+            <tr role="row">
+                <th aria-label="Rendering engine: activate to sort column descending" aria-sort="ascending" style="width: 80px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" role="columnheader" class="sorting_asc">Codigo de Aula</th>
+                <th aria-label="Browser: activate to sort column ascending" style="width: 283px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" role="columnheader" class="sorting">Capacidad</th>
+                <th aria-label="CSS grade: activate to sort column ascending" style="width: 114px;" colspan="1" rowspan="1" aria-controls="example1" tabindex="0" role="columnheader" class="sorting">Actions</th>
+            </tr>
+        </thead>
             <tbody aria-relevant="all" aria-live="polite" role="alert">
                 @foreach( $datos as $dato)
                 <tr class="odd">
-                        <td class=" ">{{ $dato->codAula }}</td>
-                        <td class=" ">{{ $dato->capacidad }}</td>
-                        <td class="" >
-                            <span class="label label-warning">{{ HTML::link('Aula/updatecID/'.$dato->codAula,'Actualizar') }}</span>
-                            <span class="label label-danger">{{ HTML::link('Aula/post_eliminar/'.$dato->codAula,'Eliminar') }}</span>
-                            <span class="label label-primary">{{ HTML::link('Aula/profile/'.$dato->codAula,'Detalles') }}</span>
-                        </td>
+                    <td class=" ">{{ $dato->codAula }}</td>
+                    <td class=" ">{{ $dato->capacidad }}</td>
+                    <td class="" >
+                        <span class="label label-warning">{{ HTML::link('Aula/updatecID/'.$dato->codAula,'Actualizar') }}</span>
+                        <span class="label label-danger">{{ HTML::link('Aula/post_eliminar/'.$dato->codAula,'Eliminar') }}</span>
+                        <span class="label label-primary">{{ HTML::link('Aula/profile/'.$dato->codAula,'Detalles') }}</span>
+                    </td>
                 </tr>
                 @endforeach
-                </tbody>
-            </table>
-                 Pagina Actual:{{ $datos->getCurrentPage()}}
-            </div>
-                {{ $datos->links()}}
-    </div><!-- /.box-body -->
+            </tbody>
+    </table>
+    Pagina Actual:{{ $datos->getCurrentPage()}}
+    {{ $datos->links()}}
+
 </div>
 @stop
