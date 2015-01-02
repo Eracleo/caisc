d5d5fd0e682f1bbe3d5b36e3fb815311fad12e8c
AyLGroup <lesly7231@gmail.com>
2015-01-01 20:31:20 -0500

    e agregado buscar por carrera y modulo

--- app/controllers/CursosCarreraTecnicaController.php
+++ app/controllers/CursosCarreraTecnicaController.php
@@ -1,11 +1,7 @@
 <?php
 
 class CursosCarreraTecnicaController extends BaseController{
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
+	
 	public function nuevo()
 	{
 		$carrera = Carrera::lists('nombre','id');
@@ -27,11 +23,36 @@ class CursosCarreraTecnicaController extends BaseController{
 
 	public function listar()
 	{ 
+		$carrera = Carrera::lists('nombre','id');
+		$modulo = Modulo::lists('nombre','id');
 		$datos = CursoTecnico::where('estado','=','1')->orderBy('id','ASC')->paginate(10);		
-		$curso_ct = CursoTecnico::where('estado','=','1')->orderBy('id','ASC')->get();
-		return View::make('Cursos_Carrera_Tecnica.index',compact("datos"),array('curso_ct'=>$curso_ct));
+		return View::make('Cursos_Carrera_Tecnica.index',compact("datos"),array('carrera'=>$carrera,'modulo'=>$modulo));
 	}
-
+	public function listarCarrera()
+	{
+		$idcarrera = Input::get('codCarrera');
+		$carrera = Carrera::lists('nombre','id');
+		$modulo = Modulo::lists('nombre','id');
+		$datos = CursoTecnico::where('codCarrera','=',$idcarrera)->where('estado','=','1')->orderBy('id','ASC')->paginate(10);		
+		return View::make("Cursos_Carrera_Tecnica.index",compact('datos'),array('carrera'=>$carrera,'modulo'=>$modulo));
+	}
+	public function listarModulo()
+	{
+		$idModulo = Input::get('modulo');
+		$carrera = Carrera::lists('nombre','id');
+		$modulo = Modulo::lists('nombre','id');
+		$datos = CursoTecnico::where('modulo','=',$idModulo)->where('estado','=','1')->orderBy('id','ASC')->paginate(10);		
+		return View::make("Cursos_Carrera_Tecnica.index",compact('datos'),array('carrera'=>$carrera,'modulo'=>$modulo));
+	}
+	public function listarAmbos()
+	{
+		$idModulo = Input::get('modulo');
+		$idcarrera = Input::get('codCarrera');
+		$carrera = Carrera::lists('nombre','id');
+		$modulo = Modulo::lists('nombre','id');
+		$datos = CursoTecnico::where('modulo','=',$idModulo)->where('codCarrera','=',$idcarrera)->where('estado','=','1')->orderBy('id','ASC')->paginate(10);		
+		return View::make("Cursos_Carrera_Tecnica.index",compact('datos'),array('carrera'=>$carrera,'modulo'=>$modulo));
+	}
 	public function ActualizarConID($id)
 	{
 		if(is_null($id))
