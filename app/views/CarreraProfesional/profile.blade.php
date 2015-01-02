f057a3c20d796f338b220c5f0ef539a7546d3f44
Gerar Francis <fryzito@users.noreply.github.com>
2015-01-02 01:18:03 -0500

    Actualizando profile CP

--- app/views/CarreraProfesional/profile.blade.php
+++ app/views/CarreraProfesional/profile.blade.php
@@ -2,20 +2,28 @@
 @section('title')
 Perfil <small>Carrera Profesional</small>
 @stop
-@section('breadcrumb')
-<li>{{ HTML::link('CarreraProfesional','Carrera Profesional') }}</li>
-<li>{{$carrera->nombre}}</li>
+@section('options')
+<li>{{HTML::link('CarreraProfesional','Listar')}}</li>
+<li>{{HTML::link('CarreraProfesional/add.html','Nueva Carrera')}}</li>
 @stop
 @section('content')
 <div class="row">
-	<div class="col-lg-3">
-		<p align="center"><b>código:</b>{{ $carrera->id }}</p>
-	</div>
+	
 	<div class="col-lg-7">
-		<p>{{ HTML::link('CarreraProfesional/updatecID/'.$carrera->id,'Editar') }} {{ HTML::link('CarreraProfesional/post_eliminar/'.$carrera->id,'Eliminar') }} </p>
-		<p><b>ID:</b>{{ $carrera->id }}</p>
-		<p><b>Nombre:</b> {{ $carrera->nombre }}</p>
-		<p><b>Descripcion:</b> {{ $carrera->descripcion }}</p>
+		<p><b>Código: </b>{{ $carrera->id }}</p>
+		<p><b>Nombre: </b> {{ $carrera->nombre }}</p>
+		<p><b>Descripcion: </b> {{ $carrera->descripcion }}</p>
+		<p>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+    		{{HTML::linkAction('CarreraProfesionalController@index', 'Retornar','',array('class'=>'col-sm-8 btn btn-primary'))}}
+    		</div>
+    		<div class="col-xs-12 col-sm-6 col-md-4">
+        	{{HTML::linkAction('CarreraProfesionalController@ActualizarConID', 'Actualizar',$carrera->id,array('class'=>'col-sm-8 btn btn-warning'))}}
+        	</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+			{{HTML::linkAction('CarreraProfesionalController@post_eliminar', 'Eliminar',$carrera->id,array('class'=>'col-sm-8 btn btn-danger'))}}
+			</div>
+		</p>
 	</div>
 </div>
 @stop
