bd136434bc48326d3b1176acf449135efc1c0701
yuresz <yuresz_14@outlook.com>
2015-01-05 06:16:11 -0500

    vista buscar matriculas CT

--- app/views/matriculaCT/index.blade.php
+++ app/views/matriculaCT/index.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.base_admin')
 @section('title')
-Registrar Matricula
+Buscar Registros disponibles para matrícula
 @stop
 @section('options')
 @stop
 @section('content')
-<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+<style>
+    #formulario {
+        float: none;
+        width: 60%; 
+    }
+</style>
+<div id='formulario' class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+    <p>Ingrese Código del Alumno para buscar matrículas disponibles en el semestre seleccionado</p>
     {{ Form::open(array('method'=> 'POST','url'=> 'matriculas_ct/listaMatricula.html','class'=>'form-horizontal','role'=>'form')) }}
         <div class="form-group">
             {{ Form::label('codAlumno','Ingrese Codigo Alumno:',array('class'=>'col-sm-4 control-label')) }}
@@ -21,7 +28,7 @@ Registrar Matricula
         </div>
         <div class="form-group">
             <div class="col-xs-12 col-sm-6 col-md-6">
-                <button class="btn btn-primary btn-block" type="submit">Buscar</button>
+                <button class="btn btn-primary btn-block" type="submit">Buscar Matrículas Disponibles</button>
             </div>
         </div>
     {{Form::close()}}    
